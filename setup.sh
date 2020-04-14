eecf9f6ef95f874cdc795712d778b764d66dfe92
James Yang <jamesyang916@gmail.com>
2020-04-14 18:14:45 -0400

    Fix change to prev dir without echo

--- setup.sh
+++ setup.sh
@@ -11,5 +11,5 @@ if [ ! -d "$gtestpath" ]; then
     git clone https://github.com/google/googletest.git $gtestpath
     cd $gtestpath 2>&1 /dev/null
     git checkout -q release-1.10.0
-    cd - 2>&1 /dev/null
+    cd ~- # change back to previous dir and no output to terminal
 fi
