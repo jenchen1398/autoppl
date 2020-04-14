f96dc1a4f068ff8372be413e8eaf3430dd194e9e
James Yang <jamesyang916@gmail.com>
2020-04-14 18:07:16 -0400

    Remove certain outputs to stdout and stupid messages

--- setup.sh
+++ setup.sh
@@ -4,16 +4,12 @@ gbenchpath="lib/benchmark"
 gtestpath="$gbenchpath/googletest"
 
 # setup google benchmark
-echo "Setting up google benchmark... "
 git submodule update --init
-echo "done"
 
 # setup googletest
-echo "Setting up googletest... "
 if [ ! -d "$gtestpath" ]; then
     git clone https://github.com/google/googletest.git $gtestpath
-    cd $gtestpath
+    cd $gtestpath 2&>1 /dev/null
     git checkout -q release-1.10.0
-    cd -
+    cd - 2&>1 /dev/null
 fi
-echo "done"
