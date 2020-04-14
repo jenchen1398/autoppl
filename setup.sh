44faa1165ad5530b0caaae249f61b593b7be73db
James Yang <jamesyang916@gmail.com>
2020-04-14 18:06:02 -0400

    Quietly checkout to release-1.10.0 tag in benchmark

--- setup.sh
+++ setup.sh
@@ -4,16 +4,16 @@ gbenchpath="lib/benchmark"
 gtestpath="$gbenchpath/googletest"
 
 # setup google benchmark
-echo -n "Setting up google benchmark... "
+echo "Setting up google benchmark... "
 git submodule update --init
 echo "done"
 
 # setup googletest
-echo -n "Setting up googletest... "
+echo "Setting up googletest... "
 if [ ! -d "$gtestpath" ]; then
     git clone https://github.com/google/googletest.git $gtestpath
     cd $gtestpath
-    git checkout release-1.10.0
+    git checkout -q release-1.10.0
     cd -
 fi
 echo "done"
